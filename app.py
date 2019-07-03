0742e6b60122ba9c2fe520e3fdd4a03c438aa9dc
Semenov Roman <3908441@gmail.com>
2019-07-03 18:36:06 +0300

    add basic auth

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
 app.config.from_object(Config)
@@ -9,5 +10,12 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 from src import models
 from src import routes
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.get(user_id)
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,38 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, login_required, current_user, logout_user
+from src.models import User
+from app import login_manager
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
-    
+@login_required
+def index():
+    return render_template("index.html", user=current_user)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+    user_id = request.form["login"]
+    user = User.query.get(user_id)
+
+    if user is None or user.password_hash != request.form["password"]:
+        return render_template("login.html", login=user_id, error="Wrong password or login")
+
+    login_user(user)
+
+    return redirect(url_for("index"))
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for("login"))
+
