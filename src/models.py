64a6c485c4b7cb766933c9106e22b5391f39c534
Semenov Roman <3908441@gmail.com>
2019-07-04 11:47:51 +0300

    add `Site` model

--- src/models.py
+++ src/models.py
@@ -1,8 +1,24 @@
-from app import db
+from enum import Enum
 from flask_login import UserMixin
+from app import db
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(128))
+
+
+class SiteStatus(Enum):
+	NEW = 0
+	PROCESSING = 1
+	BLOCKED = 2
+	GOOD_SITE = 3
+
+class Site(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String, unique=True)
+    status = db.Column(db.Enum(SiteStatus), default=SiteStatus.NEW)
+    comment = db.Column(db.Text)
+    whois_data = db.Column(db.Text)
