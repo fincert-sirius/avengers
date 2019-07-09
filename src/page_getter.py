41d575bf451c7acfeff52079c001b33695710414
Semenov Roman <3908441@gmail.com>
2019-07-09 12:35:18 +0300

    add page getter

---
+++ src/page_getter.py
@@ -0,0 +1,17 @@
+import requests
+from page import Page
+
+class Page_getter:
+	def get_page(self, url):
+		pass
+
+class Page_getter_requests(Page_getter):
+	def get_page(self, url):
+		html = requests.get(url)
+		
+		return Page(
+			url = url,
+			html = html
+		)
+
+page_getter = Page_getter_requests()
