9bbd2d8f3d556b176b3ac9f33c8d8fe4155e6757
Khaziev Danis <DanisHaz112345@gmail.com>
2019-07-09 20:41:30 +0300

    add some rules

--- src/scorer.py
+++ src/scorer.py
@@ -1,29 +1,55 @@
-from src import rules
+from src.rules import domain
+from src.rules import html
+from collections import namedtuple
 
 # All rules that will be checked
 default_rules = [
-	rules.Susp_ending(),
-	rules.Susp_words(),
-	rules.Keywords(),
-	rules.Check_minus(),
-	rules.Many_subdomains(),
+	domain.Susp_ending(),
+	domain.Susp_words(),
+	domain.Keywords(),
+	domain.Check_minus(),
+	domain.Many_subdomains(),
+	html.Check_susp_title(),
+	html.Check_susp_text(),
+	html.Check_2_auth(),
+	html.Check_pass_input(),
+	html.Check_multi_auth(),
+	html.Check_hid_input()
 ]
+
+Cause = namedtuple('Cause', ['rule', 'score'])
 
 class Scorer:
 	def __init__(self, rules):
 		self.rules = rules
 
 	def get_score(self, domain):
-		result = []
+		result = Result()
 
 		for rule in self.rules:
 			score = rule.get_score(domain)
 			if score != 0:
-				result.append({
-					'desc': rule.get_description(),
-					'score': score	
-				})
+				result.add(rule, score)
 
 		return result
+
+class Result:
+	def __init__(self, causes = []):
+		self.causes = causes
+
+	def add(self, rule, score):
+		self.causes.append(
+			Cause(rule, score)
+		)
+
+	def get_sum(self):
+		score = 0
+		for cause in self.causes:
+			score += cause.score
+
+		return score
+
+	def get_causes(self):
+		return self.causes
 
 default_scorer = Scorer(rules = default_rules)
