4559652d93ebd41f2f59845cd6b56868af402927
somvy <xenon.a@ya.ru>
2019-07-14 15:19:48 +0300

    Добавили частично handler

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/avengers'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     CSRF_ENABLED = True
