234819f86ac865d5a89e87a73160b53fc4e24ed6
Timur <timikm2002@gmail.com>
2019-07-15 16:58:27 +0300

    Fully refactored

---
+++ handler/engine.py
@@ -0,0 +1,30 @@
+import time
+import socket
+import yaml
+from ipwhois import IPWhois
+from app import db
+from models import Site
+
+def handle(domain):
+    time.sleep(15) # magic
+
+    verdict = 0
+    comment = 'Some notes'
+    category = 'lokhotron'
+
+    site = Site(url=domain, comment=comment, whois_data=get_whois(domain))
+    db.session.add(site)
+    db.session.commit()
+
+def get_whois(domain):
+    ip = get_ip(domain)
+    obj = IPWhois(ip)
+    info = obj.lookup_whois()
+    print(info)
+
+def get_ip(domain):
+    try:
+        ip = socket.gethostbyname(domain)
+        return ip
+    except:
+        return 'error'
