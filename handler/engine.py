b92c7a59adb1f55e0a17c29e9cb8404d0d1cf862
Timur <timikm2002@gmail.com>
2019-07-16 16:45:10 +0300

    New whois lib

--- handler/engine.py
+++ handler/engine.py
@@ -1,7 +1,8 @@
 import time
 import socket
 import yaml
-from ipwhois import IPWhois
+import whois
+import json
 from app import db
 from models import Site, SiteStatus
 
@@ -12,26 +13,22 @@ def handle(domain):
     comment = 'Some notes'
     category = 'lokhotron'
 
-    whois = get_whois('domain')
+    site = Site.query.filter(Site.url == domain).one_or_none()
+
+    whois = get_whois(domain)
     if whois == 'error':
         print('This web-site does not exists.')
+        db.session.delete(Site.id)
+        db.session.commit()
         return
+    site.whois_data = json.dumps(whois, ensure_ascii=False, separators=(',',':'))
+    site.status = SiteStatus.NEW
 
-    site = Site(url=domain, comment=comment, whois_data=whois, status=SiteStatus.NEW)
     db.session.add(site)
     db.session.commit()
 
 def get_whois(domain):
-    ip = get_ip(domain)
-    if ip == 'error':
-        return 'error'
-    obj = IPWhois(ip)
-    info = obj.lookup_whois()
-    return info
-
-def get_ip(domain):
     try:
-        ip = socket.gethostbyname(domain)
-        return ip
+        return whois.whois(domain)
     except:
         return 'error'
